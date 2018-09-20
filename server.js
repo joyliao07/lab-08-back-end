070b2917e60634e8ad0544499f78b625bfbb1406
Joyce Liao <joyliao07@gmail.com>
2018-09-20 09:56:31 -0700

    starter code with server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
@@ -14,10 +15,10 @@ app.get('/location', (request, response) => {
     .then(location => response.send(location));
 });
 
-// Dark Skies / weather
 app.get('/weather', getWeather);
-
 app.get('/yelp', getYelp);
+app.get('/movies', getMovies);
+
 
 function searchToLatLong(query){
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GOOGLE_API_KEY}`;
@@ -29,51 +30,63 @@ function searchToLatLong(query){
         latitude: result.body.results[0].geometry.location.lat,
         longitude: result.body.results[0].geometry.location.lng
       };
+
     });
 }
+
 
 function getWeather(request, response){
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-  return superagent.get(url)
+  return superagent
+    .get(url)
     .then(result =>{
-
       let weatherSummaries = result.body.daily.data.map(day => {return new Weather(day);});
-
       response.send(weatherSummaries);
     });
 }
-
 function Weather(day){
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
   this.forecast = day.summary;
 }
 
+
 function getYelp(request, response){
   const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
-
-  superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`);
-
-  // .then(result =>{
-  // const yelpSummaries = [];
-
-  // result.body.forEach( result => {
-  //   const summary = new Yelp(result);
-  //   yelpSummaries.push(summary);
-  // });
-  console.log('Yelp result.body: ', result.body.businesses);
-  // response.send(yelpSummaries);
-  // });
+  superagent
+    .get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(result =>{
+      let yelpSummaries = result.body.businesses.map(restaurant => {return new Business(restaurant);});
+      response.send(yelpSummaries);
+    });
+}
+function Business(result){
+  this.name = result.name;
+  this.image_url = result.image_url;
+  this.price = result.price;
+  this.rating = result.rating;
+  this.url = result.url;
 }
 
 
-// function Yelp(result){
-//   this.name = result.body.businesses.name;
-//   this.image_url = result.body.businesses.image_url;
-//   this.price = result.body.businesses.price;
-//   this.rating = result.body.businesses.rating;
-//   this.url = result.body.businesses.url;
-// }
-
+function getMovies(request, response){
+  const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&query=${request.query.data.search_query}`;
+  superagent
+    .get(url)
+    .then(result => {
+      let movieSummaries = result.body.results.map(selection => {return new Movie(selection);});
+      response.send(movieSummaries);
+    });
+}
+function Movie(selection){
+  this.title = selection.title;
+  this.released_date = selection.release_date;
+  this.vote_total = selection.vote_count;
+  this.vote_average = selection.vote_average;
+  this.popularity = selection.popularity;
+  this.image_url = selection.poster_path;
+  this.overview = selection.overview;
+}
 
 
 
